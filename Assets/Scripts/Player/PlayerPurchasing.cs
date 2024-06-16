3a81d98f86730fe9c35f2d874e2bdc9672e3d96e
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-06-16 02:04:51 -0500

    Purchasing Items Done

--- Assets/Scripts/Player/PlayerPurchasing.cs
+++ Assets/Scripts/Player/PlayerPurchasing.cs
@@ -47,6 +47,6 @@ public class PlayerPurchasing : MonoBehaviour
     {
        
         CloseShopping();
-        DeliverItem.instance.SoldRifle();
+        DeliverItem.instance.SoldSword();
     }
 }
